1df4047092887828ed55ddf04034984abb267396
nawauhailalwafa <sabirucihuyyy@gmail.com>
2024-09-02 08:19:29 +0700

    first commit

---
+++ application/views/company.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Techno Medic</title>
+    <link rel="icon" href="<?php echo base_url('assets/images/logopt.jpg'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="logo-container">
+                <img src="<?php echo base_url('assets/images/logopt.jpg'); ?>" alt="Techno Medic Logo" class="logo">
+                <span class="header-text">Techno Medic</span>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#tentang-kami">Tentang Kami</a></li>
+                    <li><a href="#lowongan">Lowongan</a></li>
+                    <li><a href="#client">Client</a></li>
+                    <li><a href="#layanan">Layanan</a></li>
+                    <li><a href="#kontak">Kontak</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <section class="main">
+        <div class="container">
+            <div class="content">
+                <h1>Sistem Informasi Manajemen Rumah Sakit</h1>
+                <h2>One-Stop Health Solution System</h2>
+                <p>Kami menyediakan jasa layanan Teknologi Informasi yang telah bermitra dengan beberapa perusahaan Teknologi Informasi yang sudah berpengalaman dan bersertifikasi.</p>
+                <a href="#layanan" class="btn">Layanan</a>
+            </div>
+            <div class="image">
+                <img src="<?php echo base_url('assets/images/logomarketing2.jpg'); ?>" alt="logo">
+            </div>
+        </div>
+    </section>
+    <section class="about-us">
+        <div class="container">
+            <div class="text-content">
+                <h1>PT. INDO TECHNO MEDIC</h1>
+                <p>PT. Indo Techno Medic adalah perusahaan yang bergerak di bidang Perdagangan Barang/Jasa khususnya dalam pengembangan Sistem Informasi Rumah Sakit baik dalam lingkungan pemerintah maupun swasta.</p>
+            </div>
+        </div>
+    </section>
+    <section id="lowongan" class="job-listing">
+        <div class="container">
+            <div class="job">
+                <h2>Lowongan Programmer</h2>
+                <ul>
+                    <li>Laki-Laki dan belum menikah.</li>
+                    <li>Pendidikan minimal SMK</li>
+                    <li>Attitude baik, tekun belajar, kemampuan bekerja dalam tim.</li>
+                    <li>Memiliki kemampuan bahasa pemrograman PHP (framework Codeigniter nilai tambah) dan database MySQL</li>
+                    <li>Paham tentang HTML/CSS/Javascript</li>
+                    <li>Pernah membuat aplikasi berbasis website</li>
+                    <li>Memiliki kemampuan desain (nilai tambah)</li>
+                </ul>
+                <p>Silahkan klik "Daftar Sekarang" untuk mengisi formulur lowongan kerja.</p>
+                <a href="#" class="apply-button">Daftar Sekarang</a>
+            </div>
+            <div class="job">
+                <h2>Lowongan Marketing</h2>
+                <ul>
+                    <li>Pria/Wanita min lulusan SMA/SMK</li>
+                    <li>Komunikatif</li>
+                    <li>Bekerja dengan orientasi target</li>
+                    <li>Supel dan Problem Solver</li>
+                    <li>Memiliki kemampuan analisis yang baik, kreatif dan inovatif</li>
+                    <li>Berpengalaman di bidang yang sama diutamakan</li>
+                    <li>Mampu mengoperasikan MS. Office</li>
+                </ul>
+                <p>Silahkan klik "Daftar Sekarang" untuk mengisi formulur lowongan kerja.</p>
+                <a href="#" class="apply-button">Daftar Sekarang</a>
+            </div>
+        </div>
+    </section>
+    <section id="client" class="client-list">
+        <div class="container">
+            <div class="client-logos">
+                <div class="client-item">
+                    <img src="<?php echo base_url('assets/images/clients/jateng.jpg'); ?>" alt="Prov. Jawa Tengah">
+                    <p>Prov. Jawa Tengah</p>
+                </div>
+                <div class="client-item">
+                    <img src="<?php echo base_url('assets/images/clients/kebumen.jpg'); ?>" alt="Kab. Kebumen">
+                    <p>Kab. Kebumen</p>
+                </div>
+                <div class="client-item">
+                    <img src="<?php echo base_url('assets/images/clients/semarang.jpg'); ?>" alt="Kab. Semarang">
+                    <p>Kab. Semarang</p>
+                </div>
+                <div class="client-item">
+                    <img src="<?php echo base_url('assets/images/clients/klaten.jpg'); ?>" alt="Kab. Klaten">
+                    <p>Kab. Klaten</p>
+                </div>
+                <div class="client-item">
+                    <img src="<?php echo base_url('assets/images/clients/kabwonogiri.jpg'); ?>" alt="Kab. Wonogiri">
+                    <p>Kab. Wonogiri</p>
+                </div>
+                <div class="client-item">
+                    <img src="<?php echo base_url('assets/images/clients/banjarnegara.jpg'); ?>" alt="Kab. Banjarnegara">
+                    <p>Kab. Banjarnegara</p>
+                </div>
+                <div class="client-item">
+                    <img src="<?php echo base_url('assets/images/clients/tegal.jpg'); ?>" alt="Kota Tegal">
+                    <p>Kota Tegal</p>
+                </div>
+                <div class="client-item">
+                    <img src="<?php echo base_url('assets/images/clients/kabbanyumas.jpg'); ?>" alt="Kab. Banyumas">
+                    <p>Kab. Banyumas</p>
+                </div>
+                <div class="client-item">
+                    <img src="<?php echo base_url('assets/images/clients/boyolali.jpg'); ?>" alt="Kab. Boyolali">
+                    <p>Kab. Boyolali</p>
+                </div>
+                <div class="client-item">
+                    <img src="<?php echo base_url('assets/images/clients/sleman.jpg'); ?>" alt="Kab. Sleman">
+                    <p>Kab. Sleman</p>
+                </div>
+                <div class="client-item">
+                    <img src="<?php echo base_url('assets/images/clients/temanggung.jpg'); ?>" alt="Kab. Temanggung">
+                    <p>Kab. Temanggung</p>
+                </div>
+                <div class="client-item">
+                    <img src="<?php echo base_url('assets/images/clients/madiun.jpg'); ?>" alt="Kab. Madiun">
+                    <p>Kab. Madiun</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="layanan" class="services">
+        <div class="container">
+            <div class="text">
+                <h2>Layanan</h2>
+                <p>Ruang lingkup produk dan layanan kami diantaranya :</p>
+                <ul>
+                    <li>Sistem Informasi Manajemen Rumah Sakit (SIMRS)</li>
+                    <li>Desktop, Web Application & Mobile Application.</li>
+                    <li>Technical Support.</li>
+                    <li>Data Center Design & Development.</li>
+                    <li>Infrastructure Fiber Optic Design & Development.</li>
+                    <li>Hardware & Software.</li>
+                    <li>System Integrator & Maintenance.</li>
+                </ul>
+            </div>
+            <div class="image">
+                <img src="<?php echo base_url('assets/images/logomarketing3.jpg'); ?>" alt="Layanan Image">
+            </div>
+        </div>
+    </section>
+    <section id="kontak" class="contact">
+        <div class="container">
+            <div class="form">
+                <form action="send_message.php" method="post">
+                    <input type="text" name="name" placeholder="Name" required>
+                    <input type="email" name="email" placeholder="Email" required>
+                    <input type="text" name="subject" placeholder="Subject" required>
+                    <textarea name="message" placeholder="Message" rows="5" required></textarea>
+                    <button type="submit">Send Message</button>
+                </form>
+            </div>
+            <div class="info">
+                <p><i class="fa fa-map-marker"></i> Lokasi: Jl. Menayu Lor, Menayu Lor, Tirtonirmolo, Kec. Kasihan, Bantul, Daerah Istimewa Yogyakarta 55184</p>
+                <p><i class="fa fa-envelope"></i> Email: info@technomedic.id</p>
+                <p><i class="fa fa-phone"></i> Telepon: +62 856 4158 4013</p>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
+</body>
+</html>
